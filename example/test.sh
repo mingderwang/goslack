e16a8e803c30ff6926c92854033c0d32b4f62a44
Ming-der Wang <ming@log4analytics.com>
2015-08-18 19:05:03 +0800

    ok for testing looop

--- example/test.sh
+++ example/test.sh
@@ -1,1 +1,5 @@
-curl -X POST -d @test.json --header "content-type: application/json" http://localhost:8091/githubUser
+#!/bin/bash
+for i in `seq 1 10000`; 
+do
+    curl -X POST -d @test.json --header "content-type: application/json" http://localhost:8091/githubUser
+done
